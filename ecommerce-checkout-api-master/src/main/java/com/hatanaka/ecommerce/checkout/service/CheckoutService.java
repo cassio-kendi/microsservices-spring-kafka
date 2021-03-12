5918b9a259c3691636479dc85de6bbef158aa4e6
cassio-kendi <cassiokendi@gmail.com>
2021-03-12 15:54:28 -0300

    Create CheckoutService interface

---
+++ ecommerce-checkout-api-master/src/main/java/com/hatanaka/ecommerce/checkout/service/CheckoutService.java
@@ -0,0 +1,11 @@
+package com.hatanaka.ecommerce.checkout.service;
+
+import com.hatanaka.ecommerce.checkout.entity.CheckoutEntity;
+import com.hatanaka.ecommerce.checkout.resource.checkout.CheckoutRequest;
+
+import java.util.Optional;
+
+public interface CheckoutService {
+
+    Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest);
+}
