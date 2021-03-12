e5200582d362605132e57ad5f63ce533a1b0cc47
cassio-kendi <cassiokendi@gmail.com>
2021-03-12 15:57:35 -0300

    Create CheckoutService interface

--- ecommerce-checkout-api-master/src/main/java/com/hatanaka/ecommerce/checkout/service/CheckoutService.java
+++ ecommerce-checkout-api-master/src/main/java/com/hatanaka/ecommerce/checkout/service/CheckoutService.java
@@ -1,11 +1,33 @@
 package com.hatanaka.ecommerce.checkout.service;
 
 import com.hatanaka.ecommerce.checkout.entity.CheckoutEntity;
+import com.hatanaka.ecommerce.checkout.entity.CheckoutItemEntity;
+import com.hatanaka.ecommerce.checkout.entity.ShippingEntity;
+import com.hatanaka.ecommerce.checkout.event.CheckoutCreatedEvent;
+import com.hatanaka.ecommerce.checkout.repository.CheckoutRepository;
 import com.hatanaka.ecommerce.checkout.resource.checkout.CheckoutRequest;
+import com.hatanaka.ecommerce.checkout.streaming.CheckoutCreatedSource;
+import com.hatanaka.ecommerce.checkout.util.UUIDUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface CheckoutService {
+@Service
+@RequiredArgsConstructor
+public class CheckoutServiceImpl implements CheckoutService {
 
-    Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest);
+    private final CheckoutRepository checkoutRepository;
+
+    @Override
+    public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
+        log.info("M=create, checkoutRequest={}", checkoutRequest);
+        final CheckoutEntity checkoutEntity = CheckoutEntity.builder()
+                .code(uuidUtil.createUUID().toString())
+                .build();
+        return Optional.of(checkoutRepository.save(checkoutEntity));
+    }
 }
